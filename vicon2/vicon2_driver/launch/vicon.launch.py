4340047a83188e616f61669a5e04a8b072c72d83
David Vargas <dvargasfr@gmail.com>
2019-12-13 13:35:52 +0100

    minor fix for linter
    Signed-off-by: David Vargas <dvargasfr@gmail.com>

--- vicon2/vicon2_driver/launch/vicon.launch.py
+++ vicon2/vicon2_driver/launch/vicon.launch.py
@@ -11,16 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# 
+#
 # Author: David Vargas Frutos <david.vargas@urjc.es>
 
-import os
-import pathlib
-import unittest
-
 from launch import LaunchDescription
-from launch import LaunchService
-from launch_ros.substitutions import ExecutableInPackage
 import launch_ros.actions
 
 
@@ -29,11 +23,11 @@ def generate_launch_description():
         launch_ros.actions.Node(
             package='vicon2_bridge', node_executable='vicon2_bridge_main',
             node_name='vicon_node', output='screen',
-            #parameters=[{'abc': 2}],
+            # parameters=[{'abc': 2}],
         )
-        #executable = ExecutableInPackage(package='vicon2_bridge', executable='simpletest')
-        #name = 'simpletest'
-        #launch_ros.actions.ExecuteProcess(
+        # executable = ExecutableInPackage(package='vicon2_bridge', executable='simpletest')
+        # name = 'simpletest'
+        # launch_ros.actions.ExecuteProcess(
         #    cmd=[executable, name,  '--stream_mode', "ClientPull",
         #                            '--datastream_hostport', "192.168.10.1:801",
         #                            '--tf_ref_frame_id', "world",
@@ -41,17 +35,3 @@ def generate_launch_description():
         #                            output='screen']
         # )
     ])
-
-"""
-<launch>
-	<!--node pkg="vicon_bridge" type="vicon_bridge" name="vicon" output="screen"-->
-	<node pkg="vicon_bridge" type="marker_tf" name="vicon" output="screen">
-		<param name="stream_mode" value="ClientPull" type="str" />
-		<param name="datastream_hostport" value="192.168.10.1:801" type="str" />
-		<!--param name="datastream_hostport" value="vicon:801" type="str" /-->
-		<param name="tf_ref_frame_id" value="world" type="str" />
-		<param name="enable_tf_broadcast" value="true" type="bool"/>
-		</node>
-	<!--node pkg="tf" type="static_transform_publisher" name="static_vicon" args="1 0 0 0 0 0 1 world vicon_marker 100"/-->
-</launch>
-"""
