a87bb60a214a0825a6720903dd43dce53ca72d4b
David Vargas <dvargasfr@gmail.com>
2020-01-09 10:23:32 +0100

    lifecycle stuff
    Signed-off-by: David Vargas <dvargasfr@gmail.com>

--- vicon2/vicon2_driver/include/vicon2_driver/vicon2_driver.hpp
+++ vicon2/vicon2_driver/include/vicon2_driver/vicon2_driver.hpp
@@ -21,37 +21,57 @@
 #include <sstream>
 #include <string>
 #include <memory>
+#include <chrono>
 
-#include "rclcpp/time.hpp"
-
+#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/empty.hpp"
 #include "vicon2_msgs/msg/marker.hpp"
 #include "vicon2_msgs/msg/markers.hpp"
 #include "vicon2_msgs/srv/vicon_calib_seg.hpp"
 #include "vicon2_msgs/srv/vicon_grab_pose.hpp"
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "rclcpp/time.hpp"
+#include "rclcpp/node_interfaces/node_logging.hpp"
+#include "rclcpp_lifecycle/lifecycle_node.hpp"
+#include "lifecycle_msgs/msg/state.hpp"
+#include "lifecycle_msgs/msg/transition.hpp"
+#include "lifecycle_msgs/srv/change_state.hpp"
+#include "lifecycle_msgs/srv/get_state.hpp"
+
 #include "tf2/buffer_core.h"
 #include "tf2_ros/transform_broadcaster.h"
 
-#include "rclcpp/node_interfaces/node_logging.hpp"
+// #include "Client.h"
+#include "DataStreamClient.h"
 
-#include "Client.h"
-
-class ViconDriver
+class ViconDriver : public rclcpp_lifecycle::LifecycleNode
 {
 public:
+
   std::string tf_ref_frame_id_;
   std::string tracked_frame_suffix_;
 
   ViconDriver();
+
+  using CallbackReturnT =
+    rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+  CallbackReturnT on_configure(const rclcpp_lifecycle::State & state);
+  CallbackReturnT on_activate(const rclcpp_lifecycle::State & state);
+  CallbackReturnT on_deactivate(const rclcpp_lifecycle::State & state);
+  CallbackReturnT on_cleanup(const rclcpp_lifecycle::State & state);
+  CallbackReturnT on_shutdown(const rclcpp_lifecycle::State & state);
+  CallbackReturnT on_error(const rclcpp_lifecycle::State & state);
   bool connect_vicon();
+  void set_settings_vicon();
   void start_vicon();
   bool stop_vicon();
 
 private:
   ViconDataStreamSDK::CPP::Client client;
-  rclcpp::Node::SharedPtr vicon_node;
+  //rclcpp::Node::SharedPtr vicon_node;
+  //std::shared_ptr<rclcpp::SyncParametersClient> parameters_client;
   rclcpp::Time now_time;
   rclcpp::Publisher<vicon2_msgs::msg::Markers>::SharedPtr marker_pub_;
   std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
@@ -66,11 +86,16 @@ private:
   unsigned int n_markers;
   unsigned int n_unlabeled_markers;
 
-  void set_settings_vicon();
+  std::string params_file_;
+  rclcpp::Parameter my_parameter_;
+
+
   void process_frame();
   void process_markers(const rclcpp::Time & frame_time, unsigned int vicon_frame_num);
   void marker_to_tf(
     vicon2_msgs::msg::Marker marker,
     int marker_num, const rclcpp::Time & frame_time);
+  std::shared_ptr<rclcpp::Client<lifecycle_msgs::srv::ChangeState>> client_change_state_;
+  rclcpp_lifecycle::LifecyclePublisher<std_msgs::msg::Empty>::SharedPtr update_pub_;
 };
 #endif  // VICON2_DRIVER__VICON2_DRIVER_HPP_
